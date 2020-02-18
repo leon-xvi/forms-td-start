626044b219701bfd035369b161e4f8b8b6e9328b
justin <walkerjt93@gmail.com>
2020-02-18 18:07:20 -0500

    handling form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,15 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f', { static: true }) signupForm: NgForm;
   defaultQuestion = 'pet';
-  answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -35,6 +42,11 @@ export class AppComponent {
   } */
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
