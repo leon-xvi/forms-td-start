7cf7a38a7f6d2eec24e3a1a44d8328ab94dbf345
Justin Walker <walkerjt93@gmail.com>
2020-02-18 16:42:49 -0500

    set a default value for my select field by creating a property in my app.component.ts, and then binding ngModel to that property with one way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', { static: true }) signupForm: NgForm;
+  defaultQuestion = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
